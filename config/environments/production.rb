55a2be7416f1a04d1c04aa4b084847c92f8bb642
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2025-05-18 14:47:57 +0500

    Temporarily allow requests from HTTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
 
   # Turn on fragment caching in view templates.
   config.action_controller.perform_caching = true
+  config.action_controller.forgery_protection_allowed_origins = [
+    "http://134.209.85.82"
+  ]
 
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
